135260e8ce31333489ff7819cf5dfcf16e345d81
RenatoStanz <stanz@tutamail.com>
2023-12-02 12:32:51 -0300

    C16 - Login and Sign-Up With Vee-Validate

--- frontend/src/views/SignUp.vue
+++ frontend/src/views/SignUp.vue
@@ -9,15 +9,29 @@
 			<div id="empty"></div>
 		</div>
 		<h1>Sign-Up</h1>
-		<form>
-			<label for="username">Username:</label>				<br />
-			<input type="text" id="username" placeholder="New Username">	<br />
+		<vee-form id="form" :validation-schema="schema" @submit="login">
 
-			<label for="password">Password:</label>				<br />
-			<input type="password" id="password" placeholder="Password">	<br />
+			<section>
 
-			<input type="submit" value="New Account">
-		</form>
+				<div>
+					<label>Username:</label>				
+					<vee-field type="text" name="username" class="input"
+						id="username" placeholder="Username" />			
+					<ErrorMessage class="required" name="username" />		
+				</div>
+
+				<div>
+					<label>Password:</label>				
+					<vee-field type="password" name="password" class="input"
+						id="password" placeholder="Password" />			
+					<ErrorMessage class="required" name="password" />		
+				</div>
+
+			</section>
+
+			<button type="submit">Login</button>
+
+		</vee-form>
 	</main>
 
 </body>
@@ -25,3 +39,21 @@
 
 <style scoped src="@/assets/css/sign_up.css">
 </style>
+
+<script>
+export default {
+	data() {
+		return {
+			schema: {
+				username: "required|min:3|max:40|alpha_dash",
+				password: "required|min:6|max:50|"
+			}
+		}
+	},
+	methods: {
+		login(values) {
+			console.log(values)
+		}
+	}
+}
+</script>
